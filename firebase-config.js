36c08cafe837959207b8ed821482330fd94715ca
Aavurton15 <nithiyaana@gmail.com>
2026-09-15 13:39:15 +0100

    Add protected parent learning area

--- firebase-config.js
+++ firebase-config.js
@@ -1,2 +1,3 @@
 // Firebase web configuration is public application identity, not a secret.
 window.BRIGHTSUM_FIREBASE_CONFIG={apiKey:'AIzaSyDwmdvxsBPvknKwgwJ-wrkCL0GQxcbFcR4',authDomain:'brightsum-3d5aa.firebaseapp.com',projectId:'brightsum-3d5aa',storageBucket:'brightsum-3d5aa.firebasestorage.app',messagingSenderId:'447724926087',appId:'1:447724926087:web:f871035340b3d02684f422'};
+window.BRIGHTSUM_APP_CHECK_SITE_KEY='6Le-8LwtAAAAAKr71QYhAID14vgnWk9b0DAghuOP';
